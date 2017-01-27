a0f6a4b4d836f85292db55f83f27875453dc2394
Darthpwner <matthewallenlin@gmail.com>
2017-01-26 20:39:30 -0800

    Fixed compilation bugs

---
+++ app/src/main/java/matthewallenlinsoftware/billutil/TipCalculatorActivity.java
@@ -0,0 +1,10 @@
+package matthewallenlinsoftware.billutil;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Darthpwner on 1/26/17.
+ */
+public class TipCalculatorActivity extends AppCompatActivity {
+
+}
