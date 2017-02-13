b42db1312cd80d56833d21a094690c2a34a506ef
Darthpwner <matthewallenlin@gmail.com>
2017-02-13 12:59:29 -0800

    Try to get buttonClick working

--- app/src/main/java/matthewallenlinsoftware/billutil/RentCalculatorActivity.java
+++ app/src/main/java/matthewallenlinsoftware/billutil/RentCalculatorActivity.java
@@ -1,13 +1,58 @@
 package matthewallenlinsoftware.billutil;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class RentCalculatorActivity extends AppCompatActivity {
+
+    // EditText
+    EditText monthlyBaseRentEditText;
+    EditText electricEditText;
+    EditText gasEditText;
+    EditText waterEditText;
+    EditText internetEditText;
+    EditText parkingSpotCostEditText;
+    EditText numberOfRoommatesEditText;
+    EditText subletterEditText;
+    EditText totalCostEditText;
+    EditText totalCostPerPersonEditText;
+
+    // Switch
+    Switch parkingSpotsSubleasedSwitch;
+
+    // Button
+    Button calculateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rent_calculator);
+
+        // Initialize EditTexts
+        monthlyBaseRentEditText = (EditText) findViewById(R.id.monthlyBaseRentEditText);
+        electricEditText = (EditText) findViewById(R.id.electricEditText);
+        gasEditText = (EditText) findViewById(R.id.gasEditText);
+        waterEditText = (EditText) findViewById(R.id.waterEditText);
+        internetEditText = (EditText) findViewById(R.id.internetEditText);
+        parkingSpotCostEditText = (EditText) findViewById(R.id.parkingSpotCostEditText);
+        numberOfRoommatesEditText = (EditText) findViewById(R.id.numberOfRoommatesEditText);
+        subletterEditText = (EditText) findViewById(R.id.subletterEditText);
+        totalCostEditText = (EditText) findViewById(R.id.totalCostEditText);
+        totalCostPerPersonEditText = (EditText) findViewById(R.id.totalCostPerPersonEditText);
+
+        // Initialize Switch
+        parkingSpotsSubleasedSwitch = (Switch) findViewById((R.id.parkingSpotSubleasedSwitch));
+
+        // Initialize Button
+        calculateButton = (Button) findViewById(R.id.calculateButton);
+    }
+
+    //Perform calculations
+    public void buttonClick() {
+        totalCostEditText.setText("50");
+        totalCostPerPersonEditText.setText("12.5");
     }
 }
