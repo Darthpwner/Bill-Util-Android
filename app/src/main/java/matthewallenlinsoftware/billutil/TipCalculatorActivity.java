b47c4778d5e480512e0134bfa9278d3d468d112c
Darthpwner <matthewallenlin@gmail.com>
2017-02-14 14:44:59 -0800

    Debug progress bar

--- app/src/main/java/matthewallenlinsoftware/billutil/TipCalculatorActivity.java
+++ app/src/main/java/matthewallenlinsoftware/billutil/TipCalculatorActivity.java
@@ -5,8 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class TipCalculatorActivity extends AppCompatActivity {
+
+    // TextView
+    TextView tipPercentageValueTextView;
 
     // Clickable + Editable EditTexts
     EditText billAmountEditText;
@@ -22,10 +27,16 @@ public class TipCalculatorActivity extends AppCompatActivity {
     Button calculateButton;
     Button setButton;
 
+    // ProgressBar
+    ProgressBar tipPercentageProgressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tip_calculator);
+
+        // Initalize TextView
+        tipPercentageValueTextView = (TextView) findViewById(R.id.tipPercentageValueTextView);
 
         // Initialize EditTexts
         billAmountEditText = (EditText) findViewById(R.id.billAmountEditText);
@@ -39,6 +50,9 @@ public class TipCalculatorActivity extends AppCompatActivity {
         // Initialize Buttons
         calculateButton = (Button) findViewById(R.id.calculateButton);
         setButton = (Button) findViewById(R.id.setButton);
+
+        // Initialize ProgressBar
+        tipPercentageProgressBar = (ProgressBar) findViewById(R.id.tipPercentageProgressBar);
     }
 
     //Perform calculations
@@ -47,7 +61,7 @@ public class TipCalculatorActivity extends AppCompatActivity {
         double numberOfPeopleAmount = Double.parseDouble(numberOfPeopleEditText.getText().toString());
 
         // Temp
-        double tipPercentage = 0.15;
+        double tipPercentage = tipPercentageProgressBar.getProgress() * .01;
         //Temp
 
         double tipAmount = billAmount * tipPercentage;
@@ -66,5 +80,12 @@ public class TipCalculatorActivity extends AppCompatActivity {
         totalEditText.setText(totalAmountAsString);
         tipAmountPerPersonEditText.setText(tipAmountPerPersonAsString);
         totalPerPersonEditText.setText(totalPerPersonAsString);
+    }
+
+    public void setButton(View v) {
+        int temp = (int) (Math.random() % 100);
+
+        tipPercentageProgressBar.setProgress(temp);
+        tipPercentageValueTextView.setText(tipPercentageProgressBar.getProgress());
     }
 }
