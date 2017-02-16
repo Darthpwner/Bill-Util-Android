370a7ea57d8c38554bcdb2d159b549c519d81ea4
Darthpwner <matthewallenlin@gmail.com>
2017-02-16 00:35:10 -0800

    Set up stuff

--- app/src/main/java/matthewallenlinsoftware/billutil/IncomeCalculatorActivity.java
+++ app/src/main/java/matthewallenlinsoftware/billutil/IncomeCalculatorActivity.java
@@ -5,19 +5,71 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class IncomeCalculatorActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    // Left side
 
-    // Spinners
+    // Clickable + Editable EditTexts
+    EditText leftIncomeEditText;
+    EditText leftBonusesEditText;
+    EditText leftRSUsEditText;
+
+    // Spinner
     Spinner leftTimeIntervalSpinner;
+
+    // EditTexts
+    EditText leftOneYearEditText;
+    EditText leftTwoYearsEditText;
+    EditText leftFiveYearsEditText;
+    EditText leftTenYearsEditText;
+    EditText leftTwentyYearsEditText;
+
+    // Right side
+
+    // Clickable + Editable EditTexts
+    EditText rightIncomeEditText;
+    EditText rightBonusesEditText;
+    EditText rightRSUsEditText;
+
+    // Spinner
     Spinner rightTimeIntervalSpinner;
+
+    // EditTexts
+    EditText rightOneYearEditText;
+    EditText rightTwoYearsEditText;
+    EditText rightFiveYearsEditText;
+    EditText rightTenYearsEditText;
+    EditText rightTwentyYearsEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_income_calculator);
 
+        // Initialize left EditTexts
+        leftIncomeEditText = (EditText) findViewById(R.id.leftIncomeEditText);
+        leftBonusesEditText = (EditText) findViewById(R.id.leftBonusesEditText);
+        leftRSUsEditText = (EditText) findViewById(R.id.leftRSUsEditText);
+
+        leftOneYearEditText = (EditText) findViewById(R.id.leftOneYearEditText);
+        leftTwoYearsEditText = (EditText) findViewById(R.id.leftTwoYearsEditText);
+        leftFiveYearsEditText = (EditText) findViewById(R.id.leftFiveYearsEditText);
+        leftTenYearsEditText = (EditText) findViewById(R.id.leftTenYearsEditText);
+        leftTwentyYearsEditText = (EditText) findViewById(R.id.leftTwentyYearsEditText);
+
+        // Initialize right EditTexts
+        rightIncomeEditText = (EditText) findViewById(R.id.rightIncomeEditText);
+        rightBonusesEditText = (EditText) findViewById(R.id.rightBonusesEditText);
+        rightRSUsEditText = (EditText) findViewById(R.id.rightRSUsEditText);
+
+        rightOneYearEditText = (EditText) findViewById(R.id.rightOneYearEditText);
+        rightTwoYearsEditText = (EditText) findViewById(R.id.rightTwoYearsEditText);
+        rightFiveYearsEditText = (EditText) findViewById(R.id.rightFiveYearsEditText);
+        rightTenYearsEditText = (EditText) findViewById(R.id.rightTenYearsEditText);
+        rightTwentyYearsEditText = (EditText) findViewById(R.id.rightTwentyYearsEditText);
+        
         // Left spinner setup
         leftTimeIntervalSpinner = (Spinner) findViewById(R.id.leftSpinner);
         leftTimeIntervalSpinner.setOnItemSelectedListener(this);
@@ -31,7 +83,7 @@ public class IncomeCalculatorActivity extends AppCompatActivity implements Adapt
         leftTimeIntervalSpinner.setAdapter(adapter);
 
         // Right spinner setup
-        
+
     }
 
     @Override
